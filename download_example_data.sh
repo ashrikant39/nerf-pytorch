8eed2217bca178d7eb6202aff53c4e3e797cb0f0
ashrikant39 <ashri@gl-login3.arc-ts.umich.edu>
2024-03-13 23:51:25 -0400

    first commit

--- download_example_data.sh
+++ download_example_data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz - /scratch/hunseok_root/hunseok2/ashri/nerf-data/
-cd /scratch/hunseok_root/hunseok2/ashri/nerf-data/
+wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz -
 mkdir -p data
 cd data
 wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/nerf_example_data.zip
